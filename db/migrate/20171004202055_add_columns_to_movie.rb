367e8277e96bce3336dac85257380049e0aafd69
Leonardo Ramos <Leo@Leonardos-MBP.iptv.microsoft.com>
2017-10-05 01:52:31 -0300

    migration update

--- db/migrate/20171004202055_add_columns_to_movie.rb
+++ db/migrate/20171004202055_add_columns_to_movie.rb
@@ -3,8 +3,8 @@ class AddColumnsToMovie < ActiveRecord::Migration[5.1]
     add_column :movies, :name, :string
     add_column :movies, :description, :text
     add_column :movies, :year, :string
-    add_column :movies, :duration, :number
-    add_column :movies, :rating, :number
+    add_column :movies, :duration, :numeric
+    add_column :movies, :rating, :numeric
     add_column :movies, :cover, :string
     add_column :movies, :trailer, :string
   end
